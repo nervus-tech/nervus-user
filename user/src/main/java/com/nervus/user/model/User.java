7cd7e56e071c886c7df030a28b5507ee5bac4144
nervus-tech <nervus.tech@gmail.com>
2025-07-16 11:18:49 +0300

    Add PostgreSQL dependency and configure datasource properties

--- user/src/main/java/com/nervus/user/model/User.java
+++ user/src/main/java/com/nervus/user/model/User.java
@@ -1,5 +1,39 @@
 package com.nervus.user.model;
 
-public class User {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "users") // Explicitly name the table to avoid reserved keyword conflict
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    // Default constructor
+    public User() {}
+
+    // Parameterized constructor
+    public User(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    // Getters and setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 }
+
